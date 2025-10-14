9ec0463a5f0b20ff3b95f1955f3e94026611ec0a
Domino0472 <134224390+Domino0472@users.noreply.github.com>
2025-10-14 13:32:24 +0200

    Update App.java

--- src/main/java/pl/dominiak/orders/App.java
+++ src/main/java/pl/dominiak/orders/App.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // 1) Load app settings
+      
         AppSettings settings = AppConfigLoader.load();
         Path ordersDir = Path.of(settings.getOrdersDirectory());
         if (!Files.isDirectory(ordersDir)) {
