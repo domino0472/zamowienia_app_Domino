44df3c5bad628c757430e50c380a9a6159669f09
Domino0472 <134224390+Domino0472@users.noreply.github.com>
2025-10-14 13:29:34 +0200

    Update OrderNumberGenerator.java

--- src/main/java/pl/dominiak/orders/util/OrderNumberGenerator.java
+++ src/main/java/pl/dominiak/orders/util/OrderNumberGenerator.java
@@ -1,11 +1,5 @@
 package pl.dominiak.orders.util;
 
-import pl.dominiak.orders.model.Customer;
-import pl.dominiak.orders.model.OrderRequest;
-import pl.dominiak.orders.model.ProductItem;
-
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 
 
 public final class OrderNumberGenerator {
